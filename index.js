5eddffb558ba7a5f65f1c144a3bf0fa17849f29c
MikeYef96 <mike.yef@gmail.com>
2021-04-18 20:47:01 +0300

    fixed bugs with module type, all data is working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const contacts = require('./contacts');
+const contacts = require('./contacts.js');
 const argv = require('yargs').argv;
 
 function invokeAction({ action, id, name, email, phone }) {
